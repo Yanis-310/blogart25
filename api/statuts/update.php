ad382ea1f49da059657293dc5490ac2377de918c
Yanis-310 <taombeyanis@gmail.com>
2025-01-28 12:25:46 +0100

    first commit

---
+++ api/statuts/update.php
@@ -0,0 +1,10 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
+
+$numStat = ctrlSaisies($_POST['numStat']);
+$libStat = ctrlSaisies($_POST['libStat']);
+
+sql_update(table: "STATUT", attributs: 'libStat = "' . $libStat . '"', where: "numStat = $numStat");
+
+header('Location: ../../views/backend/statuts/list.php');
