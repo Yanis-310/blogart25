9e4956be25d50e76488ffee7deb86ec31df68d81
Yanis-310 <taombeyanis@gmail.com>
2025-01-28 12:30:23 +0100

    a

--- api/statuts/update.php
+++ api/statuts/update.php
@@ -7,4 +7,5 @@ $libStat = ctrlSaisies($_POST['libStat']);
 
 sql_update(table: "STATUT", attributs: 'libStat = "' . $libStat . '"', where: "numStat = $numStat");
 
+
 header('Location: ../../views/backend/statuts/list.php');
