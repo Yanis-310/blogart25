0bff837c598f10b434f42c8f3c2accd159182d40
Yanis-310 <taombeyanis@gmail.com>
2025-02-04 09:25:51 +0100

    create - delete - list  de membre

--- views/backend/dashboard.php
+++ views/backend/dashboard.php
@@ -40,8 +40,8 @@ include '../../header.php';
                         <tr>
                             <td>Membres</td>
                             <td>
-                                <a href="/views/backend/members/list.php" class="btn btn-primary disabled">List</a>
-                                <a href="/views/backend/members/create.php" class="btn btn-success disabled">Create</a>
+                                <a href="/views/backend/members/list.php" class="btn btn-primary ">List</a>
+                                <a href="/views/backend/members/create.php" class="btn btn-success ">Create</a>
                                 <a href="/views/backend/members/edit.php" class="btn btn-warning disabled">Edit</a>
                                 <a href="/views/backend/members/delete.php" class="btn btn-danger disabled">Delete</a>
                             </td>
@@ -52,7 +52,7 @@ include '../../header.php';
                             <td>
                                 <a href="/views/backend/articles/list.php" class="btn btn-primary ">List</a>
                                 <a href="/views/backend/articles/create.php" class="btn btn-success ">Create</a>
-                                <a href="/views/backend/articles/edit.php" class="btn btn-warning ">Edit</a>
+                                <a href="/views/backend/articles/edit.php " class="btn btn-warning disabled">Edit</a>
                                 <a href="/views/backend/articles/delete.php" class="btn btn-danger disabled">Delete</a>
                             </td>
                             <td>En même temps que l'article : image à intégrer, gestion des mots-clés associés</td>
