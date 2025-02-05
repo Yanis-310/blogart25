c0485857e47c8a32e1d34b70d249fcde0d26bbb9
Yanis-310 <taombeyanis@gmail.com>
2025-02-05 11:29:43 +0100

    c

--- functions/query/connect.php
+++ functions/query/connect.php
@@ -1,9 +1,12 @@
 <?php
 //PDO connection
-function sql_connect(){
+function sql_connect()
+{
     global $DB;
 
-    //connect BDD with PDO using SQL_HOST, SQL_USER, SQL_PWD, SQL_DB
-    // Avec encodage UTF8
-    $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB, SQL_USER, SQL_PWD);
+    if (strpos($_SERVER['HTTP_HOST'], 'scalingo') === false) {
+        $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB . ';port=' . SQL_PORT, SQL_USER, SQL_PWD);
+    } else {
+        $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB, SQL_USER, SQL_PWD);
+    }
 }
